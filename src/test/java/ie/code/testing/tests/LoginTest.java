5f1c4d1652c3fdcaf9f8575afc8a8048747a98f8
ekyne <eoin_kyne@yahoo.com>
2026-02-03 15:21:36 +0000

    successful login test case

--- src/test/java/ie/code/testing/tests/LoginTest.java
+++ src/test/java/ie/code/testing/tests/LoginTest.java
@@ -1,2 +1,20 @@
-package ie.code.testing.tests;public class LoginTest {
+package ie.code.testing.tests;
+
+import ie.code.testing.base.BaseTest;
+import ie.code.testing.pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest {
+
+    protected String username = "Admin";
+    protected String pass = "admin123";
+
+    @Test
+    void verifyLogin(){
+        LoginPage loginPage = new LoginPage(chromeDriver);
+        loginPage.login(username, pass);
+        String currentUrl = chromeDriver.getCurrentUrl();
+        Assert.assertTrue(currentUrl.contains("dashboard"), "Login failed, Dashboard not reached");
+    }
 }
