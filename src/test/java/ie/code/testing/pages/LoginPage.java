56ad02b8ed5332dc17e97263fee1d88d9a9fd695
ekyne <eoin_kyne@yahoo.com>
2026-02-03 13:53:46 +0000

    login page login elements

--- src/test/java/ie/code/testing/pages/LoginPage.java
+++ src/test/java/ie/code/testing/pages/LoginPage.java
@@ -1,2 +1,24 @@
-package ie.code.testing.pages;public class LoginPage {
+package ie.code.testing.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    private WebDriver driver;
+    @FindBy(id = "username") private WebElement usernameField;
+    @FindBy(id = "password") private WebElement passwordField;
+    @FindBy(tagName = "button") private WebElement loginButton;
+
+    public LoginPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void login(String username, String password){
+        usernameField.sendKeys(username);
+        passwordField.sendKeys(password);
+    }
 }
